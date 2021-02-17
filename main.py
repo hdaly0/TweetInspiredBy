f0d4236cae46028b47c175ca2337f45c37ff301c
ocean <haghighidaly@gmail.com>
2021-02-17 22:28:21 +0000

    Update main runner to restart on experiencing an Exception Add logging to the main runner and a new main.log log file

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ Jan 2020
 
 Main method to run on server to load/train models and start twitter stream to reply to incoming tweets.
 """
-from twitter_methods import pull_new_inbound_tweets
+import logging
+logger = logging.getLogger("MainLogger")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler("logs/main.log", "w", "utf-8")
+handler.setFormatter(logging.Formatter("%(asctime)s: %(levelname)s: %(message)s"))
+logger.addHandler(handler)
+
+
+from twitter_methods import stream_incoming_tweets
 
 
 def main():
@@ -12,8 +20,23 @@ def main():
     Main method to run in order to run the twitter bot
     :return: void
     """
+    logger.info({
+        "message": "Starting main program"
+    })
     # Use the method to poll "mentions" of the associated twitter account as the streaming method has a bug
-    pull_new_inbound_tweets()
+    # pull_new_inbound_tweets()
+
+    # Run commands in a loop to retry if an error occurs
+    while True:
+        logger.info({"message": "Inside the continuous while loop"})
+        try:
+            # Use streaming method to wait and reply to relevant tweets
+            stream_incoming_tweets()
+        except Exception as e:
+            logger.exception({
+                "message": "An Exception was raised during the continuous running of the program",
+                "exception": str(e)
+            })
 
 
 if __name__ == "__main__":
